7ecad50431e6bf4b0efe1e635ab94f05200851a7
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-08-29 15:11:52 +1200

    create and seed db

---
+++ server/db/seeds/todos.js
@@ -0,0 +1,14 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function seed(knex) {
+  // Deletes ALL existing entries
+  await knex('todos').del()
+  await knex('todos').insert([
+    { id: 1, task: 'Do Laundry', priority: 'medium', completed: false },
+    { id: 2, task: 'Wash car', priority: 'high', completed: false },
+    { id: 3, task: 'Vacuum house', priority: 'low', completed: false },
+    { id: 4, task: 'Weed garden', priority: 'medium', completed: false },
+  ])
+}
