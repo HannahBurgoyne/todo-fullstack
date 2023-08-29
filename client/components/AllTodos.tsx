b952ea4660968d82f678333ecb7432f5f857dd5c
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-08-30 11:20:06 +1200

    fetch all todos from db

--- client/components/AllTodos.tsx
+++ client/components/AllTodos.tsx
@@ -1,16 +1,25 @@
+import { useQuery } from '@tanstack/react-query'
+import { fetchTodos } from '../apis/apiClient'
+
 function AllTodos() {
+  const { data } = useQuery(['todos'], fetchTodos)
+
   return (
     <ul className="todo-list">
-      <li /*toggle completed class*/>
-        <div className="view">
-          <input
-            className="toggle"
-            type="checkbox" /*toggle checked and not checked state*/
-          />
-          <label>Sample Task</label>
-          <button /* add onclick to delete here */ className="destroy"></button>
-        </div>
-      </li>
+      {data?.map((todo) => (
+        <li key={todo.id} /*toggle completed class*/>
+          <div className="view">
+            <input
+              className="toggle"
+              type="checkbox" /*toggle checked and not checked state*/
+            />
+            <label>{todo.task}</label>
+            <button
+              /* add onclick to delete here */ className="destroy"
+            ></button>
+          </div>
+        </li>
+      ))}
     </ul>
   )
 }
