104dd76c0ab957133c4aae6e0130c1d66dc696e3
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-08-29 16:12:53 +1200

    fix error with server environment

--- server/routes/todos.ts
+++ server/routes/todos.ts
@@ -1,1 +1,58 @@
 import express from 'express'
+import { addTodo, getAllTodos, deleteTodo, updateTodo } from '../db/db'
+
+const router = express.Router()
+
+// CREATE
+router.post('/', async (req, res) => {
+  try {
+    const newTodo = req.body
+    await addTodo(newTodo)
+    res.sendStatus(201)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message)
+    }
+  }
+})
+
+// READ
+router.get('/', async (req, res) => {
+  try {
+    const todos = await getAllTodos()
+    res.json(todos)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message)
+    }
+  }
+})
+
+// UPDATE
+router.patch('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const updatedTodo = req.body
+    await updateTodo(updatedTodo, id)
+    res.sendStatus(200)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message)
+    }
+  }
+})
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    await deleteTodo(id)
+    res.sendStatus(200)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message)
+    }
+  }
+})
+
+export default router
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 import * as Path from 'node:path'
+import * as URL from 'node:url'
+import todos from './routes/todos'
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(Path.join(__dirname, 'public')))
+server.use('/api/v1/todos', todos)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
