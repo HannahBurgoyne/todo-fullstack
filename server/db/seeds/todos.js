6a5ec47230eb48b9ed411a31df338792a18ae192
Hannah <hannah.burgoyne@gmail.com>
2023-10-17 16:18:59 +1300

    refactor seeds

--- server/db/seeds/todos.js
+++ server/db/seeds/todos.js
@@ -2,9 +2,9 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('todos').del()
   await knex('todos').insert([
-    { id: 1, task: 'Do Laundry', priority: 'medium', completed: false },
-    { id: 2, task: 'Wash car', priority: 'high', completed: false },
-    { id: 3, task: 'Vacuum house', priority: 'low', completed: false },
-    { id: 4, task: 'Weed garden', priority: 'medium', completed: false },
+    { task: 'Do Laundry', priority: 'medium', completed: false },
+    { task: 'Wash car', priority: 'high', completed: false },
+    { task: 'Vacuum house', priority: 'low', completed: false },
+    { task: 'Weed garden', priority: 'medium', completed: false },
   ])
 }
