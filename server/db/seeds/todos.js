15fa9c0bf2590239a270a3791125d5d35f464d27
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2024-01-30 10:04:38 +1300

    Change priority to number

--- server/db/seeds/todos.js
+++ server/db/seeds/todos.js
@@ -2,9 +2,9 @@ export async function seed(knex) {
   // Deletes ALL existing entries
   await knex('todos').del()
   await knex('todos').insert([
-    { task: 'Do Laundry', priority: 'medium', completed: false },
-    { task: 'Wash car', priority: 'high', completed: false },
-    { task: 'Vacuum house', priority: 'low', completed: false },
-    { task: 'Weed garden', priority: 'medium', completed: false },
+    { task: 'Do Laundry', priority: 2, completed: false },
+    { task: 'Wash car', priority: 3, completed: false },
+    { task: 'Vacuum house', priority: 1, completed: false },
+    { task: 'Weed garden', priority: 2, completed: false },
   ])
 }
